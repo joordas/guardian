37fae09cd72024f80e4571e7a5f208194c679204
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-10-20 17:43:19 -0300

    Infobox on pin click

--- src/components/map.js
+++ src/components/map.js
@@ -1,25 +1,48 @@
-/* eslint-disable no-undef */
-/* global google */
 import React from 'react'
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { compose, withProps, withStateHandlers } from "recompose"
+import { withScriptjs,
+    withGoogleMap,
+    GoogleMap,
+    Marker,
+    InfoWindow,
+} from "react-google-maps"
+
+
 
 const Map = compose(
+    withStateHandlers(() => ({
+        isOpen: false,
+      }), {
+        onToggleOpen: ({ isOpen }) => () => ({
+          isOpen: !isOpen,
+        })
+      }),
     withProps({
         googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBKFw7Jn7M3Lg5bvWQmyrPBdH4wHGAlA-M",
         loadingElement: <div style={{ height: `100%` }} />,
         containerElement: <div style={{ height: `600px` }} />,
         mapElement: <div style={{ height: `100%` }} />,
-      }),
-      withScriptjs,
-      withGoogleMap)
-      (props =>
-    <GoogleMap
-        defaultZoom={props.zoom}
-        defaultCenter={{ lat: props.lat, lng: props.lng }}
-    >
-        <Marker position={{ lat: props.markerLat, lng: props.markerLng }} />
-    </GoogleMap>
-);
+    }),
+    withScriptjs,
+    withGoogleMap)
+    (props =>
+        <GoogleMap
+            defaultZoom={props.zoom}
+            defaultCenter={{ lat: props.lat, lng: props.lng }}
+        >
+            <Marker
+                position={{ lat: props.markerLat, lng: props.markerLng }}
+                onClick={props.onToggleOpen}
+            >
+                {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+                    <div>
+                        <p>test</p>
+                    </div>
+                </InfoWindow>}
+            </Marker>
+        </GoogleMap>
+    );
+
+    
 
 export default Map;
