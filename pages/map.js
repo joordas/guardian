a40a6b63fde0f950d51a2dd44c4eecd370d9603e
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2018-10-21 12:05:13 -0300

    Render multiple markers from database

--- pages/map.js
+++ pages/map.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
+import f from '../firebase'
 import Map from '../src/components/map'
 
 class MapPage extends Component {
+
+    state = {
+        markers: [],
+    }
+
+    componentDidMount() {
+        //     f.database()
+        //   .ref('Map').child('marker')
+        //   .push({
+        //     lat: -22.9739675,
+        //     lng: -43.2258994
+        // });
+        f.database().ref('Map').child('marker').once('value').then(a => {
+            const markers = Object.values(a.val())
+            console.log(markers)
+            this.setState({ markers })
+        })
+    }
+
     render() {
         return (
             <Map
                 zoom={12}
-                lat={-22.978862}
-                lng={-43.233944}
-                markerLat={-22.978862}
-                markerLng={-43.233944} 
+                // lat={-22.978862}
+                // lng={-43.233944}
+                markers={this.state.markers}
             />
         );
     }
--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { compose, withProps, withStateHandlers } from "recompose"
 import {
     withScriptjs,
@@ -12,16 +12,28 @@ import {
 
 const Map = compose(
     withStateHandlers(() => ({
-        isOpen: false,
+        lat: null,
+        lng: null,
         markerLat: null,
         markerLng: null,
-    }), {
-            onToggleOpen: ({ isOpen }) => () => ({
-                isOpen: !isOpen,
-            }),
+        map: null
+    }),
+        {
+            // onToggleOpen: ({ isOpen }) => () => ({
+            //     isOpen: !isOpen,
+            // }),
             onMapRightClick: () => (e) => ({
                 markerLat: e.latLng.lat(),
                 markerLng: e.latLng.lng(),
+            }),
+            onMapMounted: () => ref => ({
+                map: ref,
+                lat: ref.getCenter().lat(),
+                lng: ref.getCenter().lng()
+            }),
+            onDragEnd: ({ map }) => (e) => ({
+                lat: map.getCenter().lat(),
+                lng: map.getCenter().lng()
             })
         }),
     withProps({
@@ -39,13 +51,28 @@ const Map = compose(
                 lat: props.lat || -22.978862,
                 lng: props.lng || -43.233944,
             }}
+            ref={props.onMapMounted}
             onRightClick={(e) => props.onMapRightClick(e)}
+            onDragEnd={props.onDragEnd}
         >
+            {props.markers.length && props.markers.map((marker, index) => (
+                <Marker
+                    key={`marker-${index}`}
+                    position={{ lat: marker.lat, lng: marker.lng }}
+                    onClick={props.handleMarkerClick}
+                >
+                    {props.showInfo && <InfoWindow onCloseClick={props.handleMarkerClick}>
+                        <div>
+                            <p>test</p>
+                        </div>
+                    </InfoWindow>}
+                </Marker>
+            ))}
             {props.markerLat && props.markerLng && <Marker
                 position={{ lat: props.markerLat, lng: props.markerLng }}
-                onClick={props.onToggleOpen}
+                onClick={props.handleMarkerClick}
             >
-                {props.isOpen && <InfoWindow onCloseClick={props.onToggleOpen}>
+                {props.showInfo && <InfoWindow onCloseClick={props.handleMarkerClick}>
                     <div>
                         <p>test</p>
                     </div>
@@ -54,4 +81,22 @@ const Map = compose(
         </GoogleMap>
     );
 
-export default Map;
+export default class MyMap extends PureComponent {
+
+    state = {
+        showInfo: false
+    }
+
+    handleMarkerClick = () => {
+        const { showInfo } = this.state
+        this.setState({ showInfo: !showInfo })
+    }
+
+    render() {
+        return <Map {...this.props}
+            showInfo={this.state.showInfo}
+            handleMarkerClick={this.handleMarkerClick}
+        />
+    }
+
+}
